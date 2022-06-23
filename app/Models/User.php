d2f8522db4e961e532463441490cd0925f4de009
Hassel2 <hasselprassel@mail.ru>
2022-06-23 13:05:04 +0300

    See description
    * Create users migration and access tokens migration to create table in database
    * Create user model to interact with database table

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,7 +6,11 @@ use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+<<<<<<< HEAD
 use Laravel\Sanctum\HasApiTokens;
+=======
+use Laravel\Passport\HasApiTokens;
+>>>>>>> dbc47f4 (See description)
 
 class User extends Authenticatable
 {
@@ -41,4 +45,12 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+<<<<<<< HEAD
+=======
+
+	public function tasks()
+	{
+		return $this->hasMany(Task::class);
+	}
+>>>>>>> dbc47f4 (See description)
 }
