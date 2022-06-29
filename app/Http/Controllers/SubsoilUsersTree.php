87730b37484944f9b83140ef9ecfe132c6490d80
Hassel2 <hasselprassel@mail.ru>
2022-06-29 14:17:00 +0300

    Get root subsoilusers

---
+++ app/Http/Controllers/SubsoilUsersTree.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class SubsoilUsersTree extends Controller
+{
+	public function getManagementCompanies(Request $request) {
+
+		$result = DB::table('subsoil_users')
+			->select('id', 'company')
+			->joinSub(
+				DB::table('subsoil_users')
+					->select('management_company')
+					->distinct(), 'mng_comp',
+				function ($join) {
+					$join->on('subsoil_users.id', '=', 'mng_comp.management_company');
+				}
+			)->get();	
+
+		return $this->sendResponse($result->toArray(), 'Data retrived succesfully');
+	}
+}
